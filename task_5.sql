79d202146815fc507d91bbaf4cf2e7910d06b335
Teekay <tk.kayas44@gmail.com>
2025-07-07 12:34:22 +0200

    tt

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 
-INSERT INTO customers (customer_id, customer_nama, email, address)
+INSERT INTO customer (customer_id, customer_nama, email, address)
 VALUES
 (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
